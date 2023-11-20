e880d2d32cf97eb326e408ae2ad899a76d5d0fb8
Paul Meyer <paul.meyer@mind7.fr>
2023-11-20 10:04:01 +0100

    chanched switch between splash & home

--- lib/controllers/category_controller.dart
+++ lib/controllers/category_controller.dart
@@ -1,11 +1,19 @@
 import 'package:get/get.dart';
 
+import 'package:cocktails/models/drink.dart';
+import 'package:collection/collection.dart';
+
+
 class CategoryController extends GetxController {
   final _categories = <String>[].obs;
   List<String> get categories => _categories;
 
   updateCategories(List<String> categories) {
     _categories(categories);
+  }
+
+  updateCategoriesDrink(List<Drink> categories) {
+    _categories(categories.map((element) => element.strCategory).whereNotNull().toList());
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cocktails/controllers/category_controller.dart';
 import 'package:cocktails/pages/home.dart';
 import 'package:cocktails/pages/splash.dart';
+import 'package:cocktails/services/thecocktailsdb_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
@@ -14,25 +16,45 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final CategoryController categoryController = Get.put(CategoryController());
+    final TheCocktailsDBService cocktailsDBService = Get.put(TheCocktailsDBService());
     return FutureBuilder(
-      future: Future.delayed(const Duration(seconds: 3)),
+      future: Future.wait([
+        cocktailsDBService.getCategories(),
+        Future.delayed(const Duration(seconds: 2)),
+      ]),
       builder: (context, snapshot) {
+        Widget child;
         if (snapshot.connectionState == ConnectionState.done) {
-          return GetMaterialApp(
+
+          if (snapshot.hasData) {
+            if (snapshot.data?[0] != null && snapshot.data![0].drinks != null) {
+              categoryController.updateCategoriesDrink(snapshot.data![0].drinks!);
+            }
+          }
+
+          child = GetMaterialApp(
             debugShowCheckedModeBanner: false,
             theme: ThemeData(fontFamily: 'Karla'),
-            home: const Splash(),
             getPages: [
               GetPage(
-                name: '/',
-                page: () => const HomePage(),
-                transition: Transition.fadeIn,
-              )
+                  name: '/',
+                  page: () => const HomePage(),
+                  transition: Transition.fadeIn)
             ],
           );
         } else {
-          return const Splash();
+          child = const Splash();
         }
+        return AnimatedSwitcher(
+            duration: const Duration(seconds: 1),
+            child: child,
+            transitionBuilder: (child, animation) {
+              return FadeTransition(
+                  opacity: animation,
+                  child: child,
+              );
+            });
       },
     );
   }
